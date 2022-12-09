c584eb095705857679f2917af3e14512f134ee4e
kodics <kodicsworld@gmail.com>
2022-12-09 07:05:44 -0800

    positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * more headers goes there
+ * main - Entry point
  *
- * betty style doc for function main goes there
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
